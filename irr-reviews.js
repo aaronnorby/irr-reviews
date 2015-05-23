0f35363620eccd9903958720cba9340bc386742d
Aaron Norby <aaron.norby@gmail.com>
2015-05-23 12:43:56 -0400

    Builds review object from user reviews.
    This adds promisified functions that take an object of user reviews and collects professional reviews and then spits out the kappas.

--- irr-reviews.js
+++ irr-reviews.js
@@ -2,66 +2,137 @@ var Cohen = require('./lib/cohens_kappa');
 var retrieve = require('./retrieve_tomatoes_reviews');
 var Q = require('q');
 
-/*
-var user = {'Terminator': 5, 'Speed': 3, 'Aliens': 5, 'Point Break': 2, 'Red Dawn': 2, 'Blond on Blond': 1};
-var reviewer = {'Terminator': 4, 'Speed': 2, 'Aliens': 5, 'Point Break': 3, 'Red Dawn': 3, 'Blond on Blond': 4};
+/* Originally returns: 
+* Peter Travers agreement:0.88
+* Pauline Kael agreement:0.57
+* Dave Kehr agreement:0.82
 */
 
-
-// fake professional reviewer list: 
+// fake professional reviewer list, for use in testing: 
 var reviewers = {'Peter Travers': 
   {'Rambo': 5, 
     'Point Break': 3, 
     'Speed': 2, 
     'Aliens': 5,
     'Red Dawn': 3, 
-    'Blond on Blond': 4
+    'The Big Lebowski': 5,
+    'Videodrome': 4,
+    'John Wick': 2,
+    'Zero Dark Thirty': 1,
+    'All is Lost': 5,
+    'Inside Llewyn Davis': 4,
+    'Silver Linings Playbook': 3,
+    'Predator': 4  
   }, 'Pauline Kael': 
     {'Point Break': 4,
       'Aliens': 5,
-      'Red Dawn': 2,
-      'Blond on Blond': 1
+      'The Breakfast Club': 4,
+      'Inherent Vice': 4,
+      'Red Dawn': 3,
+      'The Big Lebowski': 5,
+      'Videodrome': 3,
+      'John Wick': 1,
+      'Zero Dark Thirty': 3,
+      'All is Lost': 5,
+      'Inside Llewyn Davis': 4,
+      'Silver Linings Playbook': 3
      },
   'Dave Kehr':
   {'Point Break': 3,
+    'Speed': 2, 
+    'Aliens': 5,
+    'Red Dawn': 3, 
+    'The Big Lebowski': 5,
+    'Videodrome': 4,
+    'John Wick': 2,
+    'Zero Dark Thirty': 1,
+    'All is Lost': 5,
+    'Inside Llewyn Davis': 4
    }
 };
 
 // Sample user ratings. Not me. 
 var user = {
-  'Terminator': 5, 
+  'The Terminator': 5, 
+  'Predator': 4,
+  'Point Break': 3,
   'Speed': 3, 
-  'Aliens': 5, 
-  'Point Break': 2, 
-  'Red Dawn': 2, 
-  'Blond on Blond': 1
+  'Aliens': 5,
+  'The Breakfast Club': 5,
+  'Inherent Vice': 4,
+  'Red Dawn': 2,
+  'The Big Lebowski': 5,
+  'Videodrome': 4,
+  'John Wick': 2,
+  'Zero Dark Thirty': 1,
+  'All is Lost': 5,
+  'Inside Llewyn Davis': 4,
+  'Silver Linings Playbook': 3
 };
 
+var profReviews = {};
 
+// The following
+var titles = Object.keys(user);
+var searches = [];
+titles.forEach(function(title) {
+  var search = function() {
+    return retrieve.getSearchResult(title)
+    .then(function(searchResult){ return retrieve.getReviewLink(searchResult)})
+    .then(function(reviewLink){ return retrieve.reviewsFromReviewLink(reviewLink)})
+    .then(function(reviewArray) {
+      retrieve.parseReviewArray(title, reviewArray, profReviews, true);
+    }).catch(function(error) {
+      console.log(error);
+    });
+  };
+  searches.push(search);
+});
 
+//Create the function to be called after profReviews has been populated, then add
+//it to the stack.
+var showResults = function() {
+  //*
+  try{
+  compare(user, profReviews);
+  }catch(e) {
+    console.log(e);
+  }
+}
 
-// This chain adds Terminator reviews to the list and calculates kappas.  
-return retrieve.getSearchResult("The Terminator")
-.then(function(searchResult) { return retrieve.getReviewLink(searchResult)})
-.then(function(reviewLink) {return retrieve.reviewsFromReviewLink(reviewLink)})
-.then(function(reviewArray) {
-    reviewers = retrieve.parseReviewArray("The Terminator", reviewArray, reviewers, true);
-    compare(user, reviewers);
-  }, function(error) {
-    console.log(error)
-}).catch(function(error) {
-    console.log(error);
+searches.push(showResults);
+
+//Serialize the function calls for each movie in turn. This is necessary to avoid
+//exceeding Rotten Tom's request limit (5/s).
+var result = Q(1);
+searches.forEach(function(f) {
+  result = result.then(f);
 });
 
 
+// Returns array of reviews for single film.
+function getReviews(searchTerm) {
+  return retrieve.getSearchResult(searchTerm)
+  .then(function(searchResult){ return retrieve.getReviewLink(searchResult)})
+  .then(function(reviewLink){ return retrieve.reviewsFromReviewLink(reviewLink)})
+  .then(function(reviewArray){
+    console.log(reviewArray);
+  }, function(error){
+    console.log(error);
+  }).catch(function(err){
+    console.log(err);
+  });
+}
+
 // Calls CKJS and gets scores for all reviewers, prints them out. 
-function compare(user, reviewerObject) {
+function compare(userReviews, reviewerObject) {
   var results = [];
-  for (reviewer in reviewers) {
+  for (reviewer in reviewerObject) {
     if (reviewerObject.hasOwnProperty(reviewer)) {
-      var reviews = reviewers[reviewer];
+      var reviews = reviewerObject[reviewer];
       var k = Cohen.kappa(user, reviews, 5, 'linear');
       console.log(reviewer + " agreement:" + k);
+      
     }
   };
 }
