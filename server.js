13909407a33c364b3eebd7884fe3c1df90e72c46
Aaron Norby <aaron.norby@gmail.com>
2015-06-11 22:02:26 -0400

    Wires up irr-reviews with the UI.

--- server.js
+++ server.js
@@ -1,21 +1,78 @@
 var http = require("http"),
-    url  = require("url"),
-    path = require("path"),
-    fs = require("fs"),
-    ecstatic = require("ecstatic");
+    ecstatic = require("ecstatic"),
+    Q = require("q"),
+    irr = require("./irr-reviews");
 
 var fileserver = ecstatic({root: "./"});
 
 
 http.createServer(function(request, response) {
   if (request.method == "POST") {
-    returnToSender(request, response);
+    returnAgreement(request, response);
   } else {
     fileserver(request, response);
   }
 }).listen(8000);
 
 console.log("Server running on port 8000");
+
+
+// Assumes that what we get from the client is an array of objects, each with a
+// 'title' and 'score' key. userfyData will turn this into a format that can be
+// used by irr-reviews (ie, a single object whose keys are film titles and values
+// are scores.  At some point we need to check that the scores <=5, >=1.
+function userfyData(ratings) {
+  var user = {};
+  var len = ratings.length;
+  for (var i=0; i<len; i++) {
+    user[ratings[i]['title']] = Number(ratings[i]['score']);
+  }
+  //console.log(user);
+  return user;
+}
+
+function respond(request, response, stat, type, data) {
+  response.writeHead(stat, {
+    "Content-type": type || "text/plain"
+  });
+  response.end(data);
+}
+    
+
+function returnAgreement(req, res) {
+  var data = "";
+  req.on("data", function(chunk) {
+    data += chunk;
+  });
+  req.on("end", function() {
+    var user = userfyData(JSON.parse(data));
+    //console.log(user);
+    var resData = JSON.stringify(user);
+    //respond(req, res, 200, "application/json", resData);
+    respondIrr(req, res, user);
+  });
+  req.on("error", function(err) {
+    console.log("returnAgreement Err:" + err);
+  });
+}
+
+function respondIrr(req, res, userRevs) {
+  irr(userRevs).then(function(comparisons) {
+    console.log("In respondIrr: " + comparisons);
+    respond(req, res, 200, "application/json", JSON.stringify(comparisons));
+  },
+  function(err) {
+    console.log("in respondIrr: " + err);
+    respond(req, res, 500, null, err);
+  }).catch(function(error) {
+    console.log("In respondIrr, in catch: " + error);
+  });
+}
+
+
+
+
+// Pointless stuff after here.     
 
 function returnToSender(req, res) {
   res.writeHead(200, {
@@ -38,19 +95,7 @@ function returnToSender(req, res) {
   });
 }
 
-// Assumes that what we get from the client is an array of objects, each with a
-// 'title' and 'score' key. userfyData will turn this into a format that can be
-// used by irr-reviews (ie, a single object whose keys are film titles and values
-// are scores.  At some point we need to check that the scores <=5, >=1.
-function userfyData(ratings) {
-  var user = {};
-  var len = ratings.length;
-  for (var i=0; i<len; i++) {
-    user[ratings[i]['title']] = Number(ratings[i]['score']);
-  }
-  console.log(user);
-  // return user;
-}
+
 
 
 function mutateData(obj) {
